05d4d236c4737c98a9cba45f78b6e1ab105374b0
Seunghwan Lee <reconfiguration@protonmail.com>
2022-02-22 14:20:21 +0900

    Edit pages html

--- tagbase/urls.py
+++ tagbase/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'tagbase'
+
 urlpatterns = [
-    path('main/', views.main),
-    path('search/<str:keyword>/', views.search),
+    path('main/', views.main, name='main'),
+    path('search/<str:keyword>/', views.search, name='search'),
 ]
